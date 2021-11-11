f03de559f8849030d1fa8982595490668c595b6a
Yan Tenório da Silva <yan.silva@apece.company>
2021-11-11 15:16:16 -0300

    tasks

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* today's task */}
+      <View style={styles.tasksWrapper}>
+        
+        <Text style = {styles.sectionTitle}>Tarefas de Hoje</Text>
+        
+        <View style = {styles.items}>
+          {/* tasks will go here */}
+          <Task text= {'Task 1'}/>
+          <Task text= {'Task 2'}/>
+        
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +26,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
+
   },
+
+  tasksWrapper : {
+    padding: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle  : {
+    fontSize: 21,
+    fontWeight: 'bold'
+  },
+  
+  items : {
+    marginTop : 30,
+     
+  },
+
 });
